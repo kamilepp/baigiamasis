dcef6e28a2912d79f12a64628bd0c1576e5f5c07
Kamile <kamile.petk@gmail.com>
2022-05-27 00:19:06 +0300

    BaseTest updated

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,9 +1,11 @@
 package tests;
-
-import org.testng.annotations.AfterMethod;
+import org.testng.annotations.*;
 import org.testng.annotations.BeforeMethod;
 import utils.Driver;
+import utils.TestListener;
 
+
+@Listeners(TestListener.class)
 public class BaseTest {
 
     @BeforeMethod
