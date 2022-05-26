357b60fbc9e8fbe7c8a11519eb1d724a3c7369b2
Kamile <kamile.petk@gmail.com>
2022-05-26 20:25:21 +0300

    Unneeded imports deleted

--- src/test/java/tests/seleniumEasy/Registration.java
+++ src/test/java/tests/seleniumEasy/Registration.java
@@ -1,7 +1,6 @@
 package tests.seleniumEasy;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import tests.BaseTest;
